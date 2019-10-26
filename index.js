7a659aa9b2d991637859a78de7ac06edc8eeb1ad
Alex Fryer <afryer@alumni.stanford.edu>
2019-10-25 18:44:19 -0700

    fixed typos

--- index.js
+++ index.js
@@ -11,32 +11,32 @@ const QUIZ = {
     },
     {
       question: "Starting at age 25, what percent of your annual income should you save each year to retire comfortably at 65?",
-      choices: ["1 to 4 percent", "12 to 15 percent", "17 to 21 percent", "7 to 11 percent"],
+      choices: ["0 to 3 percent", "4 to 7 percent", "8 to 11 percent", "12 to 15 percent"],
       answer: "12 to 15 percent"
     },
     {
       question: "What should your average debt to income ratio be to retire comfortably at 65",
-      choices: ["0 to 1 percent", "5 to 7 percent", "10 to 12 percent", "2 to 3 percent"],
+      choices: ["0 to 1 percent", "2 to 3 percent", "5 to 7 percent", "10 to 12 percent"],
       answer: "2 to 3 percent"
     },
     {
       question: "What is the maximum education debt you should have as a percent of your income to retire by 65?",
-      choices: [".45 percent", "2.5 percent", ".85 percent", "5 percent"],
+      choices: [".45 percent", ".85 percent", "2.5 percent", "5 percent"],
       answer: ".85 percent"
     },
     {
       question: "On average, what % of your annual income should you spend on disability insurance?",
-      choices: ["4 percent", "9 percent", "13 percent", "19 percent"],
-      answer: "9 percent"
+      choices: [".5 percent", "1 percent", "3 percent", "5 percent"],
+      answer: "3 percent"
     },
     {
-      question: "On average, what % of your annual income should you spend on life insurance insurance?",
-      choices: ["6 percent", "9 percent", "2 percent", "11 percent"],
+      question: "On average, what % of your annual income should you spend on life insurance?",
+      choices: ["2 percent", "6 percent", "9 percent", "11 percent"],
       answer: "6 percent"
     },
     {
       question: "As you near middle age, how much of your retirement investments should be in stocks vs bonds",
-      choices: ["60% stocks, 40% bonds", "50% stocks, 50% bonds", "80% stocks, 20% bonds", "20% stocks, 80% bonds"],
+      choices: ["60% stocks, 40% bonds", "20% stocks, 80% bonds" , "80% stocks, 20% bonds", "50% stocks, 50% bonds"],
       answer: "50% stocks, 50% bonds"
     }
   ]
